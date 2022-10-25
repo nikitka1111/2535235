04f324a4edf12f79ee3a368f3b80d24690655963
nikita <75108166+nikitka1111@users.noreply.github.com>
2022-10-26 06:01:37 +0800

    Add files via upload

---
+++ Сайт Практика1/js/icon_customImage.js
@@ -0,0 +1,27 @@
+(function(){
+
+    $('.load-more').on('click', function(){
+        const btn = $(this);
+        const loader = btn.find('span');
+        $.ajax({
+             url: '/data.html',
+             type: 'GET',
+             beforeSend: function (){
+                btn.attr('disabled', true);
+                loader.addClass('d-inline-block');
+             },
+             success: function(response){
+                setTimeout(function(){
+                    loader.removeClass('d-inline-block');
+                    btn.attr('disabled', false);
+                    console.log(response)
+                }, 1000);
+             },
+             error: function(){
+                alert('Error');
+                btn.attr('disabled', false);
+             }
+        });
+    });
+
+});
